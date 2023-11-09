709a92a17a7437849d9c9bfd49604523c9f81a31
yoonsion0719 <yoonsonia0719@gmail.com>
2023-11-09 15:25:46 +0900

    10-2

--- main.c
+++ main.c
@@ -5,10 +5,21 @@
 
 int main(int argc, char *argv[]) {
 	
-	char code1='A';
-	int code2=65;
+	int i=0;
+	char str[4];
 	
-	printf("code1=%c,(%d)\n",code1,code1);
-	printf("code2=%c,(%d)\n",code2,code2);
+	str[0]='a';
+	str[1]='b';
+	str[2]='c';
+	str[3]='\0';
+	
+	printf("%s\n",str);
+	
+	
+	while (str[i]!='\0'){
+		printf("%c",str[i]);
+		i+=1;
+	}
+		
 	return 0;
 }
